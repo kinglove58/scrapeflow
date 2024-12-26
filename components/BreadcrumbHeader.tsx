b5225292394d4f7790dd60f3298c4eca6490358e
kinglove58 <obafemielijahsunday@gmail.com>
2024-12-26 21:50:02 +0100

    Auto commit at 12/26/2024, 9:50:02 PM

--- components/BreadcrumbHeader.tsx
+++ components/BreadcrumbHeader.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import React from "react";
 
 function BreadcrumbHeader() {
-  return <div>BreadcrumbHeader</div>;
+  const pathName = usePathname();
+  const path = pathName === "/" ? [""] : pathName?.split("/");
+  return <div className="flex items-center flex-start"><Brea</div>;
 }
 
 export default BreadcrumbHeader;
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -22,7 +22,8 @@ const buttonVariants = cva(
         sidebarItem:
           "gap-2 !justify-start hover:bg-accent hover:bg-primary/80 hover:text-primary-foreground",
         sidebarActiveItem:
-          "gap-2 !justify-start bg-primary text-primary-foreground hover:text-primary/90",
+          "gap-2 !justify-start bg-primary text-primary-foreground",
+        // "gap-2 !justify-start bg-primary text-primary-foreground hover:text-primary/90",
       },
       size: {
         default: "h-9 px-4 py-2",
