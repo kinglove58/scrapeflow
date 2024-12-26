42adb1bc5dbe1eba8af98889b7dee73ed401aa92
kinglove58 <obafemielijahsunday@gmail.com>
2024-12-26 20:03:43 +0100

    Implement layout and sidebar components; remove unused pages

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import DesktopSidebar from "@/components/Sidebar";
+import { Separator } from "@/components/ui/separator";
 import React from "react";
 
 interface LayoutProps {
@@ -7,10 +9,17 @@ interface LayoutProps {
 function layout({ children }: LayoutProps) {
   return (
     <div className="flex h-screen">
+      <DesktopSidebar />
       <div className="flex flex-1 min-h-screen flex-col">
         <header className="flex items-center justify-between container px-6 py-4 h-[50px]">
           scrapeflow
         </header>
+        <Separator />
+        <div className="overflow-auto">
+          <div className="flex-1 container py-4 text-accent-foreground">
+            {children}
+          </div>
+        </div>
       </div>
     </div>
   );
