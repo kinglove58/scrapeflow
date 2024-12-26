a12ffe1bb0b66839bd583753488ba0a5770ce82a
kinglove58 <obafemielijahsunday@gmail.com>
2024-12-26 22:00:03 +0100

    Auto commit at 12/26/2024, 10:00:02 PM

--- components/BreadcrumbHeader.tsx
+++ components/BreadcrumbHeader.tsx
@@ -2,11 +2,32 @@
 
 import { usePathname } from "next/navigation";
 import React from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "./ui/breadcrumb";
 
 function BreadcrumbHeader() {
   const pathName = usePathname();
-  const path = pathName === "/" ? [""] : pathName?.split("/");
-  return <div className="flex items-center flex-start"><Brea</div>;
+  const paths = pathName === "/" ? [""] : pathName?.split("/");
+  return (
+    <div className="flex items-center flex-start">
+      <Breadcrumb>
+      <BreadcrumbList>
+      {paths.map((path, index)=> (
+        <React.Fragment key={path} >
+            <BreadcrumbItem>
+            <
+            </BreadcrumbItem>
+        </React.Fragment>
+      ))}
+      </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
 }
 
 export default BreadcrumbHeader;
