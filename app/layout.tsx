7625bb6a14eed13f8ec3db76b46708114df5df5e
kinglove58 <obafemielijahsunday@gmail.com>
2024-12-31 19:40:02 +0100

    Auto commit at 12/31/2024, 7:40:02 PM

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AppProviders from "@/components/provider/AppProviders";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {" "}
-        <AppProviders>{children}</AppProviders>{" "}
-      </body>
-    </html>
+    <ClerkProvider afterSignOutUrl={"/sign-in"}>
+      <html lang="en">
+        <body className={inter.className}>
+          {" "}
+          <AppProviders>{children}</AppProviders>{" "}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
