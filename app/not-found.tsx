6ae1b8c2b95d432a4aa4832e5f5ec324269ea404
kinglove58 <obafemielijahsunday@gmail.com>
2024-12-31 15:20:03 +0100

    Auto commit at 12/31/2024, 3:20:03 PM

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,11 @@ import React from "react";
 function NotFoundPage() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center">
-      not-found
+      <div className="text-center">
+        <h1 className="text-primary mb-4 font-bold text-6xl">404</h1>
+        <h2 className="text-2xl font-semibold mb-">page not found</h2>
+      </div>
+
     </div>
   );
 }
