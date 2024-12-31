53a66ce398a235240907e46fd49378490d5b0e09
kinglove58 <obafemielijahsunday@gmail.com>
2024-12-31 15:30:03 +0100

    Auto commit at 12/31/2024, 3:30:03 PM

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,5 @@
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 function NotFoundPage() {
@@ -5,9 +7,19 @@ function NotFoundPage() {
     <div className="flex flex-col min-h-screen items-center justify-center">
       <div className="text-center">
         <h1 className="text-primary mb-4 font-bold text-6xl">404</h1>
-        <h2 className="text-2xl font-semibold mb-">page not found</h2>
+        <h2 className="text-2xl font-semibold mb-4">page not found</h2>
+        <p className="text-muted-foreground mb-8 max-w-md">
+          miss information thank you.
+        </p>
+        <div className="flex flex-col sm:flex-row  justify-center gap-4">
+          <Link
+            href="/"
+            className="justify-center flex items-center px-4 py-2 bg-primary hover:bg-primary/80 text-white rounded-md transition-colors "
+          >
+            <ArrowLeft
+          </Link>
+        </div>
       </div>
-
     </div>
   );
 }
