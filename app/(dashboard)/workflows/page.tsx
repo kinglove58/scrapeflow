3469d9df5d8445e051adea5ced5fd8c6af1c1e12
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-01 13:10:02 +0100

    Auto commit at 1/1/2025, 1:10:02 PM

--- app/(dashboard)/workflows/page.tsx
+++ app/(dashboard)/workflows/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function page() {
   return (
-    <div>page</div>
-  )
+    <div className="flex flex-1 flex-col h-full">
+      <div className="flex justify-between">
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-bold">workflows</h1>
+          <p className="text-muted-foreground">Manage your workflows</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default page;
