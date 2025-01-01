e9cc72777118bbd620ebe47d5f2838c9ad38554b
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-01 21:20:02 +0100

    Auto commit at 1/1/2025, 9:20:02 PM

--- components/provider/AppProviders.tsx
+++ components/provider/AppProviders.tsx
@@ -1,8 +1,10 @@
-"use client"
+"use client";
 
 import { ThemeProvider } from "next-themes";
+import { useState } from "react";
 
 function AppProviders({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
