b4b90b20eebc1c6b8463e2dacb0e6f3d8ed1622b
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-02 13:50:02 +0100

    Auto commit at 1/2/2025, 1:50:02 PM

--- app/(dashboard)/workflows/_components/CreateWorkflowsDialog.tsx
+++ app/(dashboard)/workflows/_components/CreateWorkflowsDialog.tsx
@@ -1,17 +1,17 @@
 "use client";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
+
 function CreateWorkflowsDialog({ triggerText }: { triggerText?: string }) {
   const [open, setOpen] = useState(false);
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-            <Button>
-                {}
-            </Button>
-        </DialogTrigger>
+      <DialogTrigger asChild>
+        <Button>{triggerText ?? "Create workflow"}</Button>
+      </DialogTrigger>
     </Dialog>
-  )
+  );
 }
 
 export default CreateWorkflowsDialog;
