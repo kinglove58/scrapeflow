37ee8ea4bade4ca1855f9aac86ebc43b82b5413e
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-03 21:00:02 +0100

    Auto commit at 1/3/2025, 9:00:02 PM

--- app/(dashboard)/workflows/_components/WorkflowCard.tsx
+++ app/(dashboard)/workflows/_components/WorkflowCard.tsx
@@ -1,10 +1,19 @@
 "use client";
 
+import { Card, CardContent } from "@/components/ui/card";
+import { workflowStatus } from "@/types/workflow";
 import { workflow } from "@prisma/client";
 import React from "react";
 
 function WorkflowCard({ workflow }: { workflow: workflow }) {
-  return <div>WorkflowCard</div>;
+  const isDraft = workflow.status === workflowStatus.DRAFT;
+  return (
+    <Card className="border border-separate shadow-sm rounded-lg overflow-hidden hover:shadow-md dark:shadow-primary/30" >
+        <CardContent>
+            <div className=""></div>
+        </CardContent>
+    </Card>
+  )
 }
 
 export default WorkflowCard;
