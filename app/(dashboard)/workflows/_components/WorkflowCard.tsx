7eadba5bff823dbd2ef8721d0e478536c1b77e06
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-04 16:25:39 +0100

    Auto commit at 1/4/2025, 4:25:38 PM

--- app/(dashboard)/workflows/_components/WorkflowCard.tsx
+++ app/(dashboard)/workflows/_components/WorkflowCard.tsx
@@ -1,22 +1,31 @@
 "use client";
 
 import { Card, CardContent } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 import { workflowStatus } from "@/types/workflow";
 import { workflow } from "@prisma/client";
-import { FileIcon } from "lucide-react";
+import { FileTextIcon, PlayIcon } from "lucide-react";
 import React from "react";
 
 function WorkflowCard({ workflow }: { workflow: workflow }) {
   const isDraft = workflow.status === workflowStatus.DRAFT;
   return (
-    <Card className="border border-separate shadow-sm rounded-lg overflow-hidden hover:shadow-md dark:shadow-primary/30" >
-        <CardContent>
-            <div className="">
-                {isDraft ? (<FileIcon className="h-5 w-5" />) }
-            </div>
-        </CardContent>
+    <Card className="border border-separate shadow-sm rounded-lg overflow-hidden hover:shadow-md dark:shadow-primary/30">
+      <CardContent>
+        <div
+          className={cn(
+            "h-5 w-5 rounded-full flex items-center justify-center bg-red-500 "
+          )}
+        >
+          {isDraft ? (
+            <FileTextIcon className="h-5 w-5" />
+          ) : (
+            <PlayIcon className="h-5 w-5 text-white" />
+          )}
+        </div>
+      </CardContent>
     </Card>
-  )
+  );
 }
 
 export default WorkflowCard;
