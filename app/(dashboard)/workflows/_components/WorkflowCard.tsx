c82671016fd1048d487b8d1c710e67dbcd2c2008
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-05 18:20:03 +0100

    Auto commit at 1/5/2025, 6:20:02 PM

--- app/(dashboard)/workflows/_components/WorkflowCard.tsx
+++ app/(dashboard)/workflows/_components/WorkflowCard.tsx
@@ -5,16 +5,24 @@ import { cn } from "@/lib/utils";
 import { workflowStatus } from "@/types/workflow";
 import { workflow } from "@prisma/client";
 import { FileTextIcon, PlayIcon } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 function WorkflowCard({ workflow }: { workflow: workflow }) {
   const isDraft = workflow.status === workflowStatus.DRAFT;
+
+  const statusColors = {
+    [workflowStatus.DRAFT]: "bg-yellow-400 text-yellow-600",
+    [workflowStatus.PUBLISHED]: "bg-primary ",
+  };
   return (
     <Card className="border border-separate shadow-sm rounded-lg overflow-hidden hover:shadow-md dark:shadow-primary/30">
-      <CardContent>
+      <CardContent className="p-4 items-center flex justify-between h-[100px]">
+        <div className="" >
         <div
           className={cn(
-            "h-5 w-5 rounded-full flex items-center justify-center bg-red-500 "
+            "h-10 w-10 rounded-full flex items-center justify-center bg-red-500 ",
+            statusColors[workflow.status as workflowStatus]
           )}
         >
           {isDraft ? (
@@ -22,6 +30,20 @@ function WorkflowCard({ workflow }: { workflow: workflow }) {
           ) : (
             <PlayIcon className="h-5 w-5 text-white" />
           )}
+        </div>
+        <div className="text-base font-bold text-muted-foreground flex items-center">
+          <Link
+            href={`/workflow/editor/${workflow.id}`}
+            className="flex items-center hover:underline"
+          >
+            {workflow.name}
+          </Link>
+          {isDraft && (
+            <span className="ml-2 px-2 py-0.5 text-xs font-medium bg-yellow-100 text-yellow-800 rounded-full">
+              Draft
+            </span>
+          )}
+        </div>
         </div>
       </CardContent>
     </Card>
