5849845a6c475e59b61bc75a97cefbeae4f0ed88
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-11 10:30:02 +0100

    Auto commit at 1/11/2025, 10:30:02 AM

--- app/workflow/_component/nodes/NodeCard.tsx
+++ app/workflow/_component/nodes/NodeCard.tsx
@@ -17,7 +17,7 @@ const {getNode, setCenter} = useReactFlow();
   return (
     <div onDoubleClick={{()={
         const node = getNode(nodeId)
-        if()
+        if( )
     }}} className="rounded-md cursor-pointer bg-background border-2 border-separate w-[420px] text-xs gap-1 flex flex-col ">
       {children}
     </div>
