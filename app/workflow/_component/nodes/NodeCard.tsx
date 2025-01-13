df79a27a67f1f801f9a46ddedb42833b8002b166
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-13 12:40:06 +0100

    Auto commit at 1/13/2025, 12:40:04 PM

--- app/workflow/_component/nodes/NodeCard.tsx
+++ app/workflow/_component/nodes/NodeCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { useReactFlow } from "@xyflow/react";
 import { ReactNode } from "react";
 
@@ -12,13 +13,21 @@ function NodeCard({
   children: ReactNode;
   isSelected: boolean;
 }) {
-const {getNode, setCenter} = useReactFlow();
+  const { getNode, setCenter } = useReactFlow();
 
   return (
-    <div onDoubleClick={{()={
-        const node = getNode(nodeId)
-        if( )
-    }}} className="rounded-md cursor-pointer bg-background border-2 border-separate w-[420px] text-xs gap-1 flex flex-col ">
+    <div
+      onDoubleClick={() => {
+        const node = getNode(nodeId);
+        if (!node) return;
+        const { position, measured } = node;
+        if (!position || !measured) return;
+      }}
+      className={cn(
+        "rounded-md cursor-pointer bg-background border-2 border-separate w-[420px] text-xs gap-1 flex flex-col ",
+        isSelected && "border-primary"
+      )}
+    >
       {children}
     </div>
   );
