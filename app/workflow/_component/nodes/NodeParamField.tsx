552cd2c0857c777ade1dce6bb6f63ce60444cbb1
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-15 12:20:03 +0100

    Auto commit at 1/15/2025, 12:20:02 PM

--- app/workflow/_component/nodes/NodeParamField.tsx
+++ app/workflow/_component/nodes/NodeParamField.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client";
+import { TaskParam, TaskParamType } from "@/types/task";
+import React from "react";
+import StringParam from "./param/StringParam";
 
-function NodeParamField() {
-  return (
-    <div>NodeParamField</div>
-  )
+function NodeParamField({ param }: { param: TaskParam }) {
+  switch (param.type) {
+    case TaskParamType.STRING:
+      return <StringParam />;
+    default:
+      return (
+        <div className="w-full">
+          <p className="text-xs text-muted-foreground">Not implemented</p>
+        </div>
+      );
+  }
 }
 
-export default NodeParamField
+export default NodeParamField;
---
+++ app/workflow/_component/nodes/param/StringParam.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StringParam() {
+  return (
+    <div>StringParam</div>
+  )
+}
+
+export default StringParam
