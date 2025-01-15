7966de19e9e35fcb75fc0942b429a03ea1508bac
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-15 12:30:04 +0100

    Auto commit at 1/15/2025, 12:30:03 PM

--- app/workflow/_component/nodes/param/StringParam.tsx
+++ app/workflow/_component/nodes/param/StringParam.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { TaskParam } from "@/types/task";
+import React, { useId } from "react";
 
-function StringParam() {
-  return (
-    <div>StringParam</div>
-  )
+interface ParamProsp {
+  param: TaskParam;
 }
 
-export default StringParam
+function StringParam({ param }: ParamProsp) {
+  const id = useId();
+  return (
+    <div className="space-y-1 p-1 w-full">
+      <Label htmlFor={id} className="text-xs flex"></Label>
+      <Input id={id} />
+    </div>
+  );
+}
+
+export default StringParam;
