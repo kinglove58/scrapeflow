9c6d4ab3be6e591ebed880ceefd41e9694f55713
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-16 14:50:03 +0100

    Auto commit at 1/16/2025, 2:50:02 PM

--- app/workflow/_component/Editor.tsx
+++ app/workflow/_component/Editor.tsx
@@ -7,6 +7,7 @@ function Editor({ workflow }: { workflow: workflow }) {
   return (
     <ReactFlowProvider>
       <div className="flex flex-col h-full w-full overflow-hidden">
+        
         <section className="flex h-full overflow-auto">
           <FlowEditor workflow={workflow} />
         </section>
---
+++ app/workflow/_component/topbar/Topbar.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react'
+
+function Topbar() {
+  return (
+    <div>Topbar</div>
+  )
+}
+
+export default Topbar
