9e42803dbd8d88a31609c6fc3acfd61162464557
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-16 13:00:02 +0100

    Auto commit at 1/16/2025, 1:00:02 PM

--- app/workflow/_component/nodes/NodeInputs.tsx
+++ app/workflow/_component/nodes/NodeInputs.tsx
@@ -10,10 +10,16 @@ function NodeInputs({ children }: { children: ReactNode }) {
 
 export default NodeInputs;
 
-export function NodeInput({ input }: { input: TaskParam }) {
+export function NodeInput({
+  input,
+  nodeId,
+}: {
+  input: TaskParam;
+  nodeId: string;
+}) {
   return (
     <div className="flex justify-start relative p-3 bg-secondary w-full">
-      <NodeParamField param={input} />
+      <NodeParamField param={input} nodeId={nodeId} />
       {!input.hideHandle && (
         <Handle
           id={input.name}
