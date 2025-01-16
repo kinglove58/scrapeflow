6396afad096c0487ec19d282fe3fb32d2e948e08
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-16 17:50:02 +0100

    Auto commit at 1/16/2025, 5:50:02 PM

--- app/workflow/_component/topbar/Topbar.tsx
+++ app/workflow/_component/topbar/Topbar.tsx
@@ -1,10 +1,17 @@
 "use client"
 
+import TooltipWrapper from '@/components/TooltipWrapper'
 import React from 'react'
 
 function Topbar() {
   return (
-    <header className="flex justify-between border-p-2 border-separate sticky w-full h-[60px] top-0 bg-background z-10 p-2 top-0"></header>
+    <header className="flex justify-between border-p-2 border-separate sticky w-full h-[60px] bg-background z-10 p-2 top-0">
+        <div className="flex gap-1 flex-1">
+            <TooltipWrapper content="Back">
+                <Button></Button>
+            </TooltipWrapper>
+        </div>
+    </header>
   )
 }
 
