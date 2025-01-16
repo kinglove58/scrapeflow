24579e61cf3b117261274fe924fe66da265397a8
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-16 15:00:04 +0100

    Auto commit at 1/16/2025, 3:00:04 PM

--- app/workflow/_component/Editor.tsx
+++ app/workflow/_component/Editor.tsx
@@ -2,12 +2,13 @@ import { workflow } from "@prisma/client";
 import React from "react";
 import { ReactFlowProvider } from "@xyflow/react";
 import FlowEditor from "./FlowEditor";
+import Topbar from "./topbar/Topbar";
 
 function Editor({ workflow }: { workflow: workflow }) {
   return (
     <ReactFlowProvider>
       <div className="flex flex-col h-full w-full overflow-hidden">
-        
+        <Topbar/>
         <section className="flex h-full overflow-auto">
           <FlowEditor workflow={workflow} />
         </section>
--- app/workflow/_component/topbar/Topbar.tsx
+++ app/workflow/_component/topbar/Topbar.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 function Topbar() {
   return (
-    <div>Topbar</div>
+    <header className="flex justify-between border-p-2 border-separate sticky w-full h-[60px] top-0 bg-background z-10 p-2 top-0"></header>
   )
 }
 
