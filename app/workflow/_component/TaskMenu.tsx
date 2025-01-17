a7248c65f836fa8c8590ae704b2aea08daf753b4
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-17 20:10:02 +0100

    Auto commit at 1/17/2025, 8:10:02 PM

--- app/workflow/_component/TaskMenu.tsx
+++ app/workflow/_component/TaskMenu.tsx
@@ -6,14 +6,21 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { TaskType } from "@/types/task";
+import { TaskRegistry } from "@/lib/workflow/task/registry";
+import { Button } from "@/components/ui/button";
 
 function TaskMenu() {
   return (
     <aside className="w-[340px] min-w-[340px] max-w-[340px] border-r-2 border-separate p-2 px-4 h-full overflow-auto ">
       <Accordion type="multiple" className="w-full">
-        <AccordionItem value="extraction" >
-          <AccordionTrigger className="font-bold" >Data Extraction</AccordionTrigger>
-          <AccordionContent></AccordionContent>
+        <AccordionItem value="extraction">
+          <AccordionTrigger className="font-bold">
+            Data Extraction
+          </AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-1">
+            <TaskMenuBtn taskType={TaskType.PAGE_TO_HTML} />
+          </AccordionContent>
         </AccordionItem>
       </Accordion>
     </aside>
@@ -21,3 +28,13 @@ function TaskMenu() {
 }
 
 export default TaskMenu;
+
+function TaskMenuBtn({ taskType }: { taskType: TaskType }) {
+  const task = TaskRegistry[taskType];
+  return (
+    <Button variant={"secondary"} className="flex justify-between">
+      <task.icon size={20} />
+      {task.label}
+    </Button>
+  );
+}
