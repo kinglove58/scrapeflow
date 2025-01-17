adcb01b7d633128524ddef6ccd624743829a1e0f
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-17 12:30:02 +0100

    Auto commit at 1/17/2025, 12:30:02 PM

--- app/workflow/_component/topbar/SaveBtn.tsx
+++ app/workflow/_component/topbar/SaveBtn.tsx
@@ -13,15 +13,19 @@ function SaveBtn({ workflowId }: { workflowId: string }) {
   const saveMutation = useMutation({
     mutationFn: UpdateWorkflow,
     onSuccess: () => {
-      toast.success("Flow saved successfully", {id})
-    }
-  })
+      toast.success("Flow saved successfully", { id: "save-workflow" });
+    },
+    onError: () => {
+      toast.error("Something went wrong", { id: "save-workflow" });
+    },
+  });
   return (
     <Button
       variant={"outline"}
       className="stroke-green-400"
       onClick={() => {
-        alert("Todo");
+        const workflowDefinition = JSON.stringify(toObject());
+        toast.loading("Saving workflow...")
       }}
     >
       <CheckIcon />
