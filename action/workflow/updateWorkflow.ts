24854a0fc8d0740aab6bd214854e8d5ee49fe064
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-17 12:20:03 +0100

    Auto commit at 1/17/2025, 12:20:02 PM

--- action/workflow/updateWorkflow.ts
+++ action/workflow/updateWorkflow.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import prisma from "@/lib/prisma";
+import { workflowStatus } from "@/types/workflow";
 import { auth } from "@clerk/nextjs/server";
 
 export async function UpdateWorkflow({
@@ -12,13 +13,29 @@ export async function UpdateWorkflow({
 }) {
   const { userId } = auth();
   if (!userId) {
-    throw new Error();
+    throw new Error("unathenticated");
   }
 
-  const workflow = await prisma.workflow.findUnique(
-    where:{
-        id,
-        userId
-    }
-  )
+  const workflow = await prisma.workflow.findUnique({
+    where: {
+      id,
+      userId,
+    },
+  });
+
+  if (!workflow) {
+    throw new Error("workflow not found");
+  }
+  if (workflow.status !== workflowStatus.DRAFT) {
+    throw new Error("workflow is not a draft");
+  }
+  await prisma.workflow.update({
+    data: {
+      definition,
+    },
+    where: {
+      id,
+      userId,
+    },
+  });
 }
--- app/workflow/_component/topbar/SaveBtn.tsx
+++ app/workflow/_component/topbar/SaveBtn.tsx
@@ -1,12 +1,21 @@
 "use client";
 
+import { UpdateWorkflow } from "@/action/workflow/updateWorkflow";
 import { Button } from "@/components/ui/button";
+import { useMutation } from "@tanstack/react-query";
 import { useReactFlow } from "@xyflow/react";
 import { CheckIcon } from "lucide-react";
 import React from "react";
+import { toast } from "sonner";
 
 function SaveBtn({ workflowId }: { workflowId: string }) {
   const { toObject } = useReactFlow();
+  const saveMutation = useMutation({
+    mutationFn: UpdateWorkflow,
+    onSuccess: () => {
+      toast.success("Flow saved successfully", {id})
+    }
+  })
   return (
     <Button
       variant={"outline"}
