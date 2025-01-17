3b1efd8e4dd0b6dd9749e0f4d8d6613082b167eb
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-17 13:50:05 +0100

    Auto commit at 1/17/2025, 1:50:03 PM

--- action/workflow/updateWorkflow.ts
+++ action/workflow/updateWorkflow.ts
@@ -3,6 +3,7 @@
 import prisma from "@/lib/prisma";
 import { workflowStatus } from "@/types/workflow";
 import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 export async function UpdateWorkflow({
   id,
@@ -38,4 +39,5 @@ export async function UpdateWorkflow({
       userId,
     },
   });
+  revalidatePath("/workflow");
 }
