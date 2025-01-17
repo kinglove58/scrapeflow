f0e59cc5b769f60335ff885363493be1739c817b
kinglove58 <obafemielijahsunday@gmail.com>
2025-01-17 14:40:02 +0100

    Auto commit at 1/17/2025, 2:40:02 PM

---
+++ lib/workflow/task/PageToHtml.tsx
@@ -0,0 +1,20 @@
+import { TaskParamType, TaskType } from "@/types/task";
+import { CodeIcon, GlobeIcon, LucideIcon } from "lucide-react";
+
+export const PageToHtmlTask = {
+  type: TaskType.PAGE_TO_HTML,
+  label: "Get Html from pages",
+  icon: (props: LucideIcon) => (
+    <CodeIcon className="stroke-rose-400" {...props} />
+  ),
+  isEntryPoint: true,
+  input: [
+    {
+      name: "web page",
+      type: TaskParamType.BROWSER_INSTANCE,
+      helperText: "e.g: https://www.google.com",
+      required: true,
+      hideHandle: true,
+    }
+  ]
+};
--- types/task.ts
+++ types/task.ts
@@ -1,9 +1,12 @@
 export enum TaskType {
   LAUNCH_BROWSER = "LAUNCH_BROWSER",
+  PAGE_TO_HTML = "PAGE_TO_HTML"
+
 }
 
 export enum TaskParamType {
   STRING = "STRING",
+  
 }
 
 export interface TaskParam{
